c3c3d4c392303afaf7a1ce8a78c692f8f0276045
Rosario Bosch <rosariobosch93@gmail.com>
2022-02-02 17:37:56 -0300

    Se modifica la ruta del SW

--- js/app.js
+++ js/app.js
@@ -1,6 +1,12 @@
+let url = window.location.href;
+let swLocation = '/cursoPWA/sw.js';
 
 if(navigator.serviceWorker) {
-    navigator.serviceWorker.register('/sw.js');
+
+    if(url.includes('localhost')) {
+        swLocation = '/sw.js';
+    } 
+    navigator.serviceWorker.register(swLocation);
 }
 
 // Referencias de jQuery
